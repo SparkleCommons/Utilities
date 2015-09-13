166f6573e33a264b4d651ff4fd1dd472f409ee0e
Michael Zanca <qrok@projectbnet.net>
2015-09-12 21:34:12 -0400

    Introduce StringUtils

---
+++ src/main/java/com/github/qrokodial/sparkle/utilities/characters/StringUtils.java
@@ -0,0 +1,24 @@
+package com.github.qrokodial.sparkle.utilities.characters;
+
+public class StringUtils {
+    /**
+     * Checks to see if a string starts with another string in a case-insensitive manner.
+     *
+     * @param haystack the main string
+     * @param needle the string to test against the main string
+     * @return true if it matches, false otherwise
+     */
+    public static boolean startsWithIgnoreCase(String haystack, String needle) {
+        if (needle.length() > haystack.length()) {
+            return false;
+        }
+
+        for (int i = 0; i < needle.length(); i++) {
+            if (!haystack.substring(i, i + 1).equalsIgnoreCase(needle.substring(i, i + 1))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
