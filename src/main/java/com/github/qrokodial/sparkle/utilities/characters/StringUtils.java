c0fa4d15427417c85d4330648806a934cd19cf3a
Michael Zanca <qrok@projectbnet.net>
2015-09-16 19:15:19 -0400

    Implement StringUtils#endsWithIgnoreCase

--- src/main/java/com/github/qrokodial/sparkle/utilities/characters/StringUtils.java
+++ src/main/java/com/github/qrokodial/sparkle/utilities/characters/StringUtils.java
@@ -21,4 +21,26 @@ public class StringUtils {
 
         return true;
     }
+
+    /**
+     * Checks to see if a string ends with another string in a case-insensitive manner.
+     *
+     * @param haystack the main string
+     * @param needle the string to test against the main string
+     * @return true if it matches, false otherwise
+     */
+    public static boolean endsWithIgnoreCase(String haystack, String needle) {
+        if (needle.length() > haystack.length()) {
+            return false;
+        }
+
+        for (int i = 0; i < needle.length(); i++) {
+            int start = haystack.length() - needle.length() + i;
+            if (!haystack.substring(start, start + 1).equalsIgnoreCase(needle.substring(i, i + 1))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
