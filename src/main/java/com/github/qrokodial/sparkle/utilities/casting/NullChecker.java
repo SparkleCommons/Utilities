dfdce934d1633bce737835e9598e4f563dabb4a2
Michael Zanca <qrok@projectbnet.net>
2015-09-23 00:41:06 -0400

    Introduce NullChecker

---
+++ src/main/java/com/github/qrokodial/sparkle/utilities/casting/NullChecker.java
@@ -0,0 +1,21 @@
+package com.github.qrokodial.sparkle.utilities.casting;
+
+public class NullChecker {
+    /**
+     * Throws an exception if the object is null.
+     *
+     * @param object the object to check if null
+     * @param exception the exception to throw if the object is null
+     * @param <T>
+     * @param <E>
+     * @return the object, assuming it wasn't null
+     * @throws E the exception originally passed to this function
+     */
+    public static <T, E extends Exception> T notNullOrThrow(T object, E exception) throws E {
+        if (object == null) {
+            throw exception;
+        }
+
+        return object;
+    }
+}
