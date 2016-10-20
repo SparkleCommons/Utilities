daa5df444e6575686e76a7e3f3c51c17fc45d54f
Mike Zanca <qrokodial@gmail.com>
2016-10-20 18:17:55 -0400

    Begin implementing DateUtils

---
+++ src/main/java/com/qrokodial/sparkle/utilities/time/DateUtils.java
@@ -0,0 +1,9 @@
+package com.qrokodial.sparkle.utilities.time;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class DateUtils {
+    public static final DateFormat DATE_ONLY = new SimpleDateFormat("MM/dd/yyyy");
+    public static final DateFormat TIMESTAMP = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss aaa");
+}
